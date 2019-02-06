43cc37d4466c8ce207c684eea7bf9b651b2d3815
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-06 18:56:27 -0500

    Updated get request to include equipment type string

--- routes/otherRoutes.js
+++ routes/otherRoutes.js
@@ -37,7 +37,7 @@ router.get('/', authenticate, async (req, res) => {
       .select(
         'equipmentType.id',
         'equipment.id',
-        'equipment.type',
+        'equipmentType.type',
         'equipment.equipmentImage',
         'equipment.broken',
         'user.name',
@@ -119,13 +119,59 @@ router.get('/resolved', authenticate, async (req, res) => {
       .innerJoin('statusTypes', 'boardLog.status', 'statusTypes.statusID')
       .innerJoin('user', 'boardLog.boardUser', 'user.id')
       .innerJoin('role', 'user.role', 'role.id')
-      .select()
+      .select(
+        'equipment.id',
+        'equipmentType.type',
+        'equipment.broken',
+        'schoolLog.equipmentID',
+        'role.role'
+      )
       .where({ statusID: 1 });
-    res.status(responseStatus.success).json(types);
+    // const equipment = await db
+    //   .from('equipment')
+    //   .select()
+    //   .innerJoin('equipmentType', 'equipment.id', 'equipmentType.id')
+    //   .where({ 'equipment.id': id });
+    // const boardLog = await db
+    //   .from('boardLog')
+    //   .select(
+    //     'boardLog.id',
+    //     'boardLog.equipmentID',
+    //     'boardLog.status',
+    //     'boardLog.boardUser',
+    //     'boardLog.boardComment',
+    //     'boardLog.created_at',
+    //     'equipmentType.type',
+    //     'user.name',
+    //     'role.role'
+    //   )
+    //   .innerJoin('equipmentType', 'boardLog.equipmentID', 'equipmentType.id')
+    //   .innerJoin('user', 'boardLog.boardUser', 'user.id')
+    //   .innerJoin('role', 'user.role', 'role.id')
+    //   .where({ 'boardLog.equipmentID': id });
+    // const schoolLog = await db
+    //   .from('schoolLog')
+    //   .select(
+    //     'schoolLog.id',
+    //     'schoolLog.equipmentID',
+    //     'schoolLog.broken',
+    //     'schoolLog.user',
+    //     'schoolLog.comment',
+    //     'schoolLog.created_at',
+    //     'equipmentType.type',
+    //     'user.name',
+    //     'role.role'
+    //   )
+    //   .innerJoin('equipmentType', 'schoolLog.equipmentID', 'equipmentType.id')
+    //   .innerJoin('user', 'schoolLog.user', 'user.id')
+    //   .innerJoin('role', 'user.role', 'role.id')
+    //   .where({ 'schoolLog.equipmentID': id });
+    res.status(responseStatus.success).json([equipment, schoolLog, board]);
   } catch (error) {
+    console.log('ERR', error);
     res
       .status(responseStatus.serverError)
-      .json({ errorMessage: 'Unable to get that equipment ID.' });
+      .json({ errorMessage: 'Unable to complete that request.' });
   }
 });
 
