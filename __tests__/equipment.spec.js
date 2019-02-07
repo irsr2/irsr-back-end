5b9743eb2166c7b6b0fbcb1e5f231dac794dd12a
Nicholas O'Ferrall <nickoferrall@gmail.com>
2019-02-07 15:54:56 -0500

    Cleaned config and tests

--- __tests__/equipment.spec.js
+++ __tests__/equipment.spec.js
@@ -1,9 +1,4 @@
-const request = require('supertest');
-const express = require('express');
 const db = require('../data/dbConfig');
-const equipment = require('../routes/equipmentRoutes');
-
-const app = express();
 
 describe('Equipment table', () => {
   it('Should insert provided equipment', async () => {
